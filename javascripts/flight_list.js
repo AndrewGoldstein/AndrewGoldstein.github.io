ac51edbb33118722903cdd5884f049e052618b37
Andrew Goldstein <goldstein@urx.com>
2016-01-20 11:42:33 -0800

    experiment with moving javascript to specific javascripts folder

---
+++ javascripts/flight_list.js
@@ -0,0 +1,27 @@
+//flight list
+
+var flights =[
+{outbound:"Friday",
+o1:18,
+o2:20,
+ret:"Sunday",
+r1:19,
+r2:21,
+home:"SFO", 
+dest:"SEA"}, {
+outbound:"Friday",
+o1:20,
+o2:22,
+ret:"Sunday",
+r1:21,
+r2:23,
+home:"SFO", 
+dest:"SEA"},{
+outbound:"Friday",
+o1:22,
+o2:6,
+ret:"Monday",
+r1:7,
+r2:10,
+home:"SEA", 
+dest:"JFK"}];
